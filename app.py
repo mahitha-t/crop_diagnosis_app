4fdb42515743eae6e9d5d820a282f24d8b716f2d
mt862 <mt862@cornell.edu>
2025-07-25 00:01:25 -0400

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import os
 
 app = Flask(__name__)
-model = torch.load("best.pt", map_location=torch.device("cpu"), , weights_only=False)
+model = torch.load("best.pt", map_location=torch.device("cpu"), weights_only=False)
 model = model['model']
 model.eval()
 
