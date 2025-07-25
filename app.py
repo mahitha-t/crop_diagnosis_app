519dfe53980f6fd59994976e00583a0debebf733
mt862 <mt862@cornell.edu>
2025-07-25 14:19:23 -0400

    update tuple

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def index():
         if file:
             img = preprocess_image(file)
             with torch.no_grad():
-                pred = model(img)
+                if isinstance(pred, tuple):
+                    pred = pred[0]  
                 prediction = torch.argmax(pred, dim=1).item()
     
     return render_template('index.html', prediction= prediction)
